647513eea5687d690c34392ae1b7a1d89e37fc06
SHURUOUQ <95751205+SHURUOUQ@users.noreply.github.com>
2021-12-10 20:15:31 +0900

    null
    new one

---
+++ ReallyJustATest_NoNeedtoSave/ReallyJustATest_NoNeedtoSave/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReallyJustATest_NoNeedtoSave
+{
+	class Program
+	{
+		static void 전투(string[] args)
+		{
+		}
+	}
+
+	Pubic class 검사
+	{
+		float HP = 100f;
+		int ATK = 80;
+
+		Public void 내려찍기(Monster monster)
+		{
+			monster.HP = monster.HP - ATK;
+		}
+
+
+		class 캐릭터생성
+
+		{
+			검사 검사1 = new 검사();
+			검사1.내려찍기();
+		}
+
+		class Monster
+		{
+			Public float HP = 1000f;
+
+		}
+
+}
